efa6d747d864fd0996a55040a656bbda768dd340
noner <pawel495@interia.pl>
2022-11-20 11:53:01 +0000

    Setting up the repository

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Daily_Challenge"
+include(":androidApp")
+include(":shared")
